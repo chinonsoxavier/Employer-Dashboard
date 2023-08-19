b2c849d6ca49a89621ed7aa49752beb7392d9e68
chinonsoxavier <101524269+chinonsoxavier@users.noreply.github.com>
2023-08-19 07:57:39 +0100

    Update MobileDashboard.jsx

--- src/Components/MobileDashboard.jsx
+++ src/Components/MobileDashboard.jsx
@@ -81,6 +81,7 @@ const QuickStatsHeaderText = styled.span`
 const QuickStatsWrapper = styled.div`
 gap:8px;
 width:100%;
+height:100%;
 `
 const QuickStats = styled.div`
   background: #eee;
@@ -120,7 +121,7 @@ const DashboardFunds = styled.div`
   width: 100%;
   max-height:130px;
   box-sizing: border-box;
-  flex:2;
+  flex:1;
   ${newlmobile({ padding: "15px 6px" })};
   ${smobile({ padding: "15px 10px",marginTop:'0' })};
 `;
